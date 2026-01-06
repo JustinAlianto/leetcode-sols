252ab28dd5c9ce4242e56dbcb4c7bc8477a2532a
JustinAlianto <63857586+JustinAlianto@users.noreply.github.com>
2026-01-06 17:11:21 -0500

    LeetCode Grind!

--- pandas/string-methods/1517.find-users-with-valid-e-mails.py
+++ pandas/string-methods/1517.find-users-with-valid-e-mails.py
@@ -6,4 +6,17 @@ def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
         users["mail"].str.match(r"^[A-Za-z][A-Za-z0-9_\.\-]*@leetcode(\?com)?\.com$")
     ]
 
+    # Regex:
+    # - '^': start of string
+    # - '[A-Za-z]': first character is a letter
+    # - '[A-Za-z0-9_\.\-]*': remaining username characters (0 or more)
+    #   - '\.' and '\-': literal dot and dash
+    #   - '*': 0 or more occurrences
+    # - '@leetcode': literal domain prefix
+    # - '(\?com)?': optional ?com
+    #   - '\?': literal ?
+    #   - '?': group is optional (0 or 1 time)
+    # - '\.com': literal .com
+    # - '$': end of string
+
     return valid_emails_df
